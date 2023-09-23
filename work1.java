47ecdf176c5ad83efa19d541aacdb6bfc5d648ab
Mike <mchlkiselev@gmail.com>
2023-09-23 14:50:58 +0300

    java works

---
+++ work1.java
@@ -0,0 +1,29 @@
+public class work1 {
+    /*
+     * Дан массив двоичных чисел, например [1,1,0,1,1,1],
+     * вывести максимальное количество подряд идущих 1.
+     */
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 1, 0, 1, 1, 1 };
+        System.out.println(method(arr));
+    }
+
+    static int method(int[] arr) {
+        int count = 0;
+        int maxCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count++;
+            } else {
+                if (maxCount < count) {
+                    maxCount = count;
+                }
+                count = 0;
+            }
+        }
+        if (maxCount < count) {
+            maxCount = count;
+        }
+        return maxCount;
+    }
+}
